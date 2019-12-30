9b5e8b1fc312f961a833e1000673be48e0ac3d09
yangwb <a12345678>
2019-12-30 11:50:56 +0800

    init project

---
+++ springcloud-alibaba-nacos/src/test/java/cn/ywb/springcloudalibaba/gateway/SpringcloudAlibabaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ywb.springcloudalibaba.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAlibabaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
